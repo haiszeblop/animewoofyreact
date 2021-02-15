1a7cf7679e1815211eacb47c3d7f3c6fe4ae5a10
haiszeblop <haiszeblop>
2021-02-15 03:34:04 +0000

    Initial commit

---
+++ components/card/Container.js
@@ -0,0 +1,49 @@
+import tw from "twin.macro";
+import Card from "./Card";
+import { useSelector } from "react-redux";
+import PagiNation from "../PagiNation";
+import Loader from "../Loader/Loader";
+function Container({ Data = [], heading, page }) {
+  const { theme, loading } = useSelector((state) => state);
+  return loading ? (
+    <Loader />
+  ) : (
+    Data.length>0?
+    <>
+      <div tw="mt-28 lg:mt-0">
+        <span
+          className={`${theme.text.selected}`}
+          tw="px-8 flex flex-col font-light  text-3xl"
+        >
+          {heading}
+        </span>
+        <span className={`${theme.text.selected}`} tw=" capitalize px-8 font-thin text-xl">
+          {heading=="Showing Results for"?page[0]:heading == "Genres" ?page[0]: "Anime"}
+        </span>
+      </div>
+
+      <div tw="grid grid-cols-2  w-full px-10 my-6  gap-10  justify-center  md:grid-cols-3 lg:grid-cols-4 lg:px-16 lg:my-16   xl:grid-cols-5 xl:gap-8">
+        {Data?.map((item, index) => (
+          <Card {...item} key={index} />
+        ))}
+      </div>
+      {page ? <PagiNation page={page} heading={"Page"} /> : null}
+    </>:<div  className={` flex flex-col h-screen  w-full text-lg`}>
+      <div className={`h-full ${theme.text.notselected} flex flex-col justify-center items-center`}>
+
+    <div className="w-full flex  flex-col justify-start items-center">
+        <img
+          width={400}
+          src={theme.theme == "dark" ? "/404dark.svg" : "/404light.svg"}
+          />
+      </div>
+      <span className="py-4">
+
+      Nothing found for&nbsp;<span className={`${theme.text.selected} capitalize text-xl font-bold`}>{page?.[0]}</span>
+      </span>
+      </div>
+          </div>
+  );
+}
+
+export default Container;
